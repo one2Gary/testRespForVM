d2c964dd05bbf7a040ecbbe161cc26e80025fd04
Shi <YShi@floridabar.org>
2022-06-22 15:51:18 -0400

    gary first test for git for newLaravelProject

---
+++ resources/js/components/VueComponent.vue
@@ -0,0 +1,10 @@
+<template>
+    <div>
+
+                    <div>
+                        this is the first vue page
+                    </div>
+
+    </div>
+</template>
+
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Laravel for Vue page</title>
+        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <!-- CSRF Token -->
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+
+        <!-- Scripts -->
+        <script src="{{ asset('js/app.js') }}" defer></script>
+        <script src="{{asset('js/main.js')}}"></script>
+
+        <!-- Fonts -->
+        <link rel="dns-prefetch" href="//fonts.gstatic.com">
+        <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+
+        <!-- Styles -->
+        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+
+
+    </head>
+    <body>
+            <div id="app" >
+                <div class="container">
+                    <ExampleComponent></ExampleComponent>
+                </div>
+                <div>
+                   test3
+                </div>
+            </div>
+
+    </body>
+</html>
